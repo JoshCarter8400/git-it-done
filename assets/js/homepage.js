3e1a7bd52d333845328e13ea2168db5cfcfe6424
Josh Carter <joshcarter8400@gmail.com>
2020-09-08 12:27:22 -0400

    working on display function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,36 @@
+var userFormE1 = document.querySelector("#user-form");
+var nameInputE1 = document.querySelector("#username")
+
+
 var getUserRepos = function(user) {
+
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a repo request to the url
     fetch(apiUrl).then(function(repsonse) {
         repsonse.json().then(function(data) {
-            console.log(data)
+            displayRepos(data, user);
         });
     });
 };
-getUserRepos("microsoft");
+getUserRepos();
+
+var formSubmitHandler = function(event) {
+    // get the value from input element 
+    var username = nameInputE1.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputE1.value = "";
+    } else {
+        alert("Please enter a Github Username")
+    }
+}
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+userFormE1.addEventListener("submit", formSubmitHandler);
